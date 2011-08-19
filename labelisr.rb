02fdc76f1ec8eab6824467d8f1e25bdfd834cd54
Matt Heath <mheath@vax.co.uk>
2011-08-19 15:14:30 +0100

    initial commit - read labels from a GitHub repository

---
+++ labelisr.rb
@@ -0,0 +1,28 @@
+# Labelisr
+#   Maintain labels in GitHub Issues for one or more repositories
+#
+# Author:: Matt Heath <hi@mattheath.com>
+# License:: MIT <http://www.opensource.org/licenses/mit-license.php>
+
+require 'net/http'
+require 'net/https'
+
+user = ''
+repo = ''
+pass = ''
+
+STDOUT.sync = true
+
+http = Net::HTTP.new('api.github.com', 443)
+http.use_ssl = true
+http.start do |http|
+   req = Net::HTTP::Get.new("/repos/#{user}/#{repo}/labels")
+
+   # we make an HTTP basic auth by passing the
+   # username and password
+   req.basic_auth user, pass
+
+   resp, data = http.request(req)
+   
+   puts resp.body
+end
