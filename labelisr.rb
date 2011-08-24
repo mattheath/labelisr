a6adb3d3d1a9bc4604469fce87a62e48b4e357d3
Matt Heath <mheath@vax.co.uk>
2011-08-24 13:41:02 +0100

    use Trollop for option parsing, move username pass and repo to command line options

--- labelisr.rb
+++ labelisr.rb
@@ -6,20 +6,34 @@
 # Author:: Matt Heath <hi@mattheath.com>
 # License:: MIT <http://www.opensource.org/licenses/mit-license.php>
 
+require 'trollop'
 require 'json'
 require 'net/http'
 require 'net/https'
 
-user = ''
-repo = ''
-pass = ''
-
 STDOUT.sync = true
+
+opts = Trollop::options do
+	opt :user, "User name", :short => "-u", :type => :string
+	opt :password, "Password for your GitHub account, optional", :short => "-p", :type => :string
+	opt :repo, "Repository to work on", :type => :string
+end
+
+user = opts[:user]
+repo = opts[:repo]
+
+puts "Username: #{user}"
+if opts.has_key?(:password_given)
+	pass = opts[:password]
+else
+	print "Please enter your password: "
+	pass = STDIN.gets
+end
 
 http = Net::HTTP.new('api.github.com', 443)
 http.use_ssl = true
 http.start do |http|
-	req = Net::HTTP::Get.new("/repos/#{user}/#{repo}/labels")
+	req = Net::HTTP::Get.new("/repos/#{repo}/labels")
 
 	# use HTTP basic auth by passing the
 	# username and password
